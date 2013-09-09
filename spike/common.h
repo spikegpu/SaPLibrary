c79e23439d3ec22c613165fd58b6fe06766784ce
Radu Serban <serban@engr.wisc.edu>
2013-09-09 13:25:26 +0000

    Restructured Spike library.

---
+++ spike/common.h
@@ -0,0 +1,102 @@
+#ifndef SPIKE_COMMON_H
+#define SPIKE_COMMON_H
+
+
+
+#define ALWAYS_ASSERT
+
+#ifdef WIN32
+typedef long long int64_t;
+#endif
+
+
+
+// ----------------------------------------------------------------------------
+// If ALWAYS_ASSERT is defined, we make sure that  assertions are triggered 
+// even if NDEBUG is defined.
+// ----------------------------------------------------------------------------
+#ifdef ALWAYS_ASSERT
+// If NDEBUG is actually defined, remember this so
+// we can restore it.
+#  ifdef NDEBUG
+#    define NDEBUG_ACTIVE
+#    undef NDEBUG
+#  endif
+// Include the assert.h header file here so that it can
+// do its stuff while NDEBUG is guaranteed to be disabled.
+#  include <assert.h>
+// Restore NDEBUG mode if it was active.
+#  ifdef NDEBUG_ACTIVE
+#    define NDEBUG
+#    undef NDEBUG_ACTIVE
+#  endif
+#else
+// Include the assert.h header file using whatever the
+// current definition of NDEBUG is.
+#  include <assert.h>
+#endif
+
+
+// ----------------------------------------------------------------------------
+
+
+namespace spike {
+
+const unsigned int BLOCK_SIZE = 512;
+
+const unsigned int MAX_GRID_DIMENSION = 32768;
+
+const unsigned int CRITICAL_THRESHOLD = 70;
+
+enum SolverType {
+	BiCGStab,
+	BiCGStab2
+};
+
+enum SolverMethod {
+	LU_UL,
+	LU_only
+};
+
+enum PrecondMethod {
+	Spike,
+	Block
+};
+
+void kernelConfigAdjust(int &numThreads, int &numBlockX, const int numThreadsMax) {
+	if (numThreads > numThreadsMax) {
+		numBlockX = (numThreads + numThreadsMax - 1) / numThreadsMax;
+		numThreads = numThreadsMax;
+	}
+}
+
+void kernelConfigAdjust(int &numThreads, int &numBlockX, int &numBlockY, const int numThreadsMax, const int numBlockXMax) {
+	kernelConfigAdjust(numThreads, numBlockX, numThreadsMax);
+	if (numBlockX > numBlockXMax) {
+		numBlockY = (numBlockX + numBlockXMax - 1) / numBlockXMax;
+		numBlockX = numBlockXMax;
+	}
+}
+
+template<typename T>
+struct Multiplier: public thrust::unary_function<T, T>
+{
+	__host__ __device__
+	T operator() (thrust::tuple<T, T> tu) {
+		return thrust::get<0>(tu) * thrust::get<1>(tu);
+	}
+};
+
+template<typename T>
+struct ExpOp: public thrust::unary_function<T, T>
+{
+	__host__ __device__
+	T operator() (T a) {
+		return exp(a);
+	}
+};
+
+} // namespace spike
+
+
+#endif
