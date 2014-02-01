7e2878d9a9d17152361bc3592e51155308e89f88
Radu Serban <serban@engr.wisc.edu>
2014-02-01 10:09:36 -0600

    Add driver options for specifying absolute tolerance (default = 0).
    Use options --relTol=TOL and --absTol=TOL to specify the relative and absolute tolerances, respectively.
    For backward compatibility, relative tolerance can still be specified using '-t' and '--tolerance'.

--- examples/synthetic_banded/driver_banded.cu
+++ examples/synthetic_banded/driver_banded.cu
@@ -48,11 +48,11 @@ using std::vector;
 
 // ID values to identify command line arguments
 enum {OPT_HELP, OPT_VERBOSE, OPT_PART,
-      OPT_TOL, OPT_MAXIT,
+      OPT_RTOL, OPT_ATOL, OPT_MAXIT,
       OPT_DROPOFF_FRAC, 
       OPT_BAND,
       OPT_OUTFILE, OPT_PRECOND,
-      OPT_SAFE_FACT};
+      OPT_KRYLOV, OPT_SAFE_FACT};
 
 // Table of CSimpleOpt::Soption structures. Each entry specifies:
 // - the ID for the option (returned from OptionId() during processing)
@@ -62,8 +62,10 @@ enum {OPT_HELP, OPT_VERBOSE, OPT_PART,
 CSimpleOptA::SOption g_options[] = {
 	{ OPT_PART,          "-p",                   SO_REQ_CMB },
 	{ OPT_PART,          "--num-partitions",     SO_REQ_CMB },
-	{ OPT_TOL,           "-t",                   SO_REQ_CMB },
-	{ OPT_TOL,           "--tolerance",          SO_REQ_CMB },
+	{ OPT_RTOL,          "-t",                   SO_REQ_CMB },
+	{ OPT_RTOL,          "--tolerance",          SO_REQ_CMB },
+	{ OPT_RTOL,          "--relTol",             SO_REQ_CMB },
+	{ OPT_ATOL,          "--absTol",             SO_REQ_CMB },
 	{ OPT_MAXIT,         "-i",                   SO_REQ_CMB },
 	{ OPT_MAXIT,         "--max-num-iterations", SO_REQ_CMB },
 	{ OPT_DROPOFF_FRAC,  "-d",                   SO_REQ_CMB },
@@ -73,6 +75,8 @@ CSimpleOptA::SOption g_options[] = {
 	{ OPT_OUTFILE,       "-o",                   SO_REQ_CMB },
 	{ OPT_OUTFILE,       "--output-file",        SO_REQ_CMB },
 	{ OPT_PRECOND,       "--precond-method",     SO_REQ_CMB },
+	{ OPT_KRYLOV,        "-k",                   SO_REQ_CMB },
+	{ OPT_KRYLOV,        "--krylov-method",      SO_REQ_CMB },
 	{ OPT_SAFE_FACT,     "--safe-fact",          SO_NONE    },
 	{ OPT_VERBOSE,       "-v",                   SO_NONE    },
 	{ OPT_VERBOSE,       "--verbose",            SO_NONE    },
@@ -258,6 +262,7 @@ GetProblemSpecs(int             argc,
                 spike::Options& opts)
 {
 	numPart = -1;
+	verbose = false;
 
 	// Create the option parser and pass it the program arguments and the array
 	// of valid options. Then loop for as long as there are arguments to be
@@ -280,8 +285,11 @@ GetProblemSpecs(int             argc,
 			case OPT_PART:
 				numPart = atoi(args.OptionArg());
 				break;
-			case OPT_TOL:
+			case OPT_RTOL:
 				opts.relTol = atof(args.OptionArg());
+				break;
+			case OPT_ATOL:
+				opts.absTol = atof(args.OptionArg());
 				break;
 			case OPT_MAXIT:
 				opts.maxNumIterations = atoi(args.OptionArg());
@@ -315,6 +323,30 @@ GetProblemSpecs(int             argc,
 						opts.precondType = spike::Spike;
 					else if(precond == "1" || precond == "BLOCK")
 						opts.precondType = spike::Block;
+					else if(precond == "2" || precond == "NONE")
+						opts.precondType = spike::None;
+					else
+						return false;
+				}
+				break;
+			case OPT_KRYLOV:
+				{
+					string kry = args.OptionArg();
+					std::transform(kry.begin(), kry.end(), kry.begin(), ::toupper);
+					if (kry == "0" || kry == "BICGSTAB")
+						opts.solverType = spike::BiCGStab;
+					else if (kry == "1" || kry == "GMRES")
+						opts.solverType = spike::GMRES;
+					else if (kry == "2" || kry == "CG")
+						opts.solverType = spike::CG;
+					else if (kry == "3" || kry == "CR")
+						opts.solverType = spike::CR;
+					else if (kry == "4" || kry == "BICGSTAB1")
+						opts.solverType = spike::BiCGStab1;
+					else if (kry == "5" || kry == "BICGSTAB2")
+						opts.solverType = spike::BiCGStab2;
+					else if (kry == "6" || kry == "MINRES")
+						opts.solverType = spike::MINRES;
 					else
 						return false;
 				}
@@ -346,17 +378,44 @@ GetProblemSpecs(int             argc,
 		cout << "Problem size: " << N << " "<<k <<" "<<d<<endl;
 		if (fileSol.length() > 0)
 			cout << "Sol file:    " << fileSol << endl;
-		cout << "Using " << numPart << (numPart ==1 ? " partition." : " partitions.") << endl;
-		cout << "Iterative solver: " << (opts.solverType == spike::BiCGStab2 ? "BiCGStab2" : "BiCGStab") << endl;
+		cout << "Iterative solver: ";
+		switch (opts.solverType) {
+		case spike::BiCGStab:
+			cout << "BiCGStab (Cusp)" << endl; break;
+		case spike::GMRES:
+			cout << "GMRES (Cusp)" << endl; break;
+		case spike::CG:
+			cout << "CG (Cusp)" << endl; break;
+		case spike::CR:
+			cout << "CR (Cusp)" << endl; break;
+		case spike::BiCGStab1:
+			cout << "BiCGStab1 (Spike::GPU)" << endl; break;
+		case spike::BiCGStab2:
+			cout << "BiCGStab2 (Spike::GPU)" << endl; break;
+		case spike::MINRES:
+			cout << "MINRES (Spike::GPU)" << endl; break;
+		}
 		cout << "Relative tolerance: " << opts.relTol << endl;
+		cout << "Absolute tolerance: " << opts.absTol << endl;
 		cout << "Max. iterations: " << opts.maxNumIterations << endl;
-		cout << "Preconditioner: " << (opts.precondType == spike::Spike ? "SPIKE" : "BLOCK DIAGONAL") << endl;
-		cout << "Factorization method: LU - UL" << endl;
-		if (opts.dropOffFraction > 0)
-			cout << "Drop-off fraction: " << opts.dropOffFraction << endl;
-		else
-			cout << "No drop-off." << endl;
-		cout << (opts.safeFactorization ? "Use safe factorization." : "Use non-safe fast factorization.") << endl;
+		cout << "Preconditioner: ";
+		switch (opts.precondType) {
+		case spike::Spike:
+			cout << "SPIKE" << endl; break;
+		case spike::Block:
+			cout << "BLOCK DIAGONAL" << endl; break;
+		case spike::None:
+			cout << "NONE" << endl; break;
+		}
+		if (opts.precondType != spike::None) {
+			cout << "Using " << numPart << (numPart ==1 ? " partition." : " partitions.") << endl;
+			cout << "Factorization method: LU - UL" << endl;
+			if (opts.dropOffFraction > 0)
+				cout << "Drop-off fraction: " << opts.dropOffFraction << endl;
+			else
+				cout << "No drop-off." << endl;
+			cout << (opts.safeFactorization ? "Use safe factorization." : "Use non-safe fast factorization.") << endl;
+		}
 		cout << endl << endl;
 	}
 
@@ -373,19 +432,6 @@ void ShowUsage()
 {
 	cout << "Usage:  driver_mm [OPTIONS]" << endl;
 	cout << endl;
-	cout << " -p=NUM_PARTITIONS" << endl;
-	cout << " --num-partitions=NUM_PARTITIONS" << endl;
-	cout << "        Specify the number of partitions (default 1)." << endl;
-	cout << " -t=TOLERANCE" << endl;
-	cout << " --tolerance=TOLERANCE" << endl;
-	cout << "        Use TOLERANCE for BiCGStab stopping criteria (default 1e-6)." << endl;
-	cout << " -i=ITERATIONS" << endl;
-	cout << " --max-num-iterations=ITERATIONS" << endl;
-	cout << "        Use at most ITERATIONS for BiCGStab (default 100)." << endl;
-	cout << " -d=FRACTION" << endl;
-	cout << " --drop-off-fraction=FRACTION" << endl;
-	cout << "        Drop off-diagonal elements such that FRACTION of the matrix" << endl;
-	cout << "        Frobenius norm is ignored (default 0.0 -- i.e. no drop-off)." << endl;
 	cout << " -b SIZE BW DD" << endl;
 	cout << " --banded-synthetic SIZE BW DD" << endl;
 	cout << "        Use a synthetic banded matrix of size SIZE, half-bandwidth BW," << endl;
@@ -393,12 +439,29 @@ void ShowUsage()
 	cout << " -o=OUTFILE" << endl;
 	cout << " --output-file=OUTFILE" << endl;
 	cout << "        Write the solution to the file OUTFILE (MatrixMarket format)." << endl;
+	cout << " -p=NUM_PARTITIONS" << endl;
+	cout << " --num-partitions=NUM_PARTITIONS" << endl;
+	cout << "        Specify the number of partitions (default 1)." << endl;
+	cout << " -t=TOLERANCE" << endl;
+	cout << " --tolerance=TOLERANCE" << endl;
+	cout << " --relTol=TOLERANCE" << endl;
+	cout << "        Use relative tolerance TOLERANCE for Krylov stopping criteria (default 1e-6)." << endl;
+	cout << " --absTol=TOLERANCE" << endl;
+	cout << "        Use absolute tolerance TOLERANCE for Krylov stopping criteria (default 0)." << endl;
+	cout << " -i=ITERATIONS" << endl;
+	cout << " --max-num-iterations=ITERATIONS" << endl;
+	cout << "        Use at most ITERATIONS for Krylov solver (default 100)." << endl;
+	cout << " -d=FRACTION" << endl;
+	cout << " --drop-off-fraction=FRACTION" << endl;
+	cout << "        Drop off-diagonal elements such that FRACTION of the matrix" << endl;
+	cout << "        Frobenius norm is ignored (default 0.0 -- i.e. no drop-off)." << endl;
 	cout << " --safe-fact" << endl;
 	cout << "        Use safe LU-UL factorization." << endl; 
 	cout << " --precond-method=METHOD" << endl;
 	cout << "        Specify the preconditioner to be used" << endl;
-	cout << "        METHOD=0 or METHOD=SPIKE                for using SPIKE preconditioner.  This is the default." << endl;
-	cout << "        METHOD=1 or METHOD=BLOCK                for using Block preconditionera." << endl;
+	cout << "        METHOD=0 or METHOD=SPIKE         SPIKE preconditioner.  This is the default." << endl;
+	cout << "        METHOD=1 or METHOD=BLOCK         Block-diagonal preconditioner." << endl;
+	cout << "        METHOD=2 or METHOD=NONE          no preconditioner." << endl;
 	cout << " -? -h --help" << endl;
 	cout << "        Print this message and exit." << endl;
 	cout << endl;
