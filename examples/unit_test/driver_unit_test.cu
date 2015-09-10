8c1446fc86e0105bba87b164585be1751713b2b7
Ang Li <ali28@wisc.edu>
2015-09-10 01:41:42 -0500

    Add googletest thirdparty framework

--- examples/unit_test/driver_unit_test.cu
+++ examples/unit_test/driver_unit_test.cu
@@ -22,3 +22,8 @@ TEST(GraphTest, TestReturn) {
 
     EXPECT_DOUBLE_EQ(1.0, graph.getTimeDB());
 }
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
